6efd76c2c26eb6adca488c918caf7216f152adb3
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-04-24 21:04:17 +0200

    ha

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-const departureKey = "8ade30c6140f4c08b5ac38690f524819";
-const departureUrl = `http://api.sl.se/api2/realtimedeparturesv4.json?key=${departureKey}&siteid=9181&timewindow=30`;
 const stopLookupKey = "6c0ea6a5673d479b99c6287e75f0c15e"
 const stopLookupUrl = `http://api.sl.se/api2/typeahead.json?key=${stopLookupKey}&searchstring=alby&stationsonly=true`;
+const proxyUrl = "https://cors-anywhere.herokuapp.com/"
 
-fetch(departureUrl, {
-  mode: "no-cors",
-})
+
+
+
+/* 
+function reqListener () {
+  console.log(this.responseText);
+}
+
+var oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", stopLookupUrl);
+oReq.send();
+*/
+
+/*
+fetch(proxyUrl + stopLookupUrl)
 .then((response) => {
-return response.json();
+  return response.json();
 })
 .then((data) => {
-console.log(data);
-});
-
-
+  let albyStationId = data.ResponseData[0].SiteId;
+  
+  const departureKey = "8ade30c6140f4c08b5ac38690f524819";
+  const departureUrl = `http://api.sl.se/api2/realtimedeparturesv4.json?key=${departureKey}&siteid=${albyStationId}&timewindow=30`;
+  
+  fetch(proxyUrl + departureUrl)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+})
+*/
