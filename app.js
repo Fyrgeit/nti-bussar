76cf79955d94eaf330ea191da86178991764ab22
Fyrgeit (Måns Henriksson) <90310668+Fyrgeit@users.noreply.github.com>
2022-04-26 12:37:09 +0200

    jsonp

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const stopLookupKey = "6c0ea6a5673d479b99c6287e75f0c15e"
-const stopLookupUrl = `http://api.sl.se/api2/typeahead.json?key=${stopLookupKey}&searchstring=åkersberga&stationsonly=true`;
+const stopLookupUrl = `https://api.sl.se/api2/typeahead.json?key=${stopLookupKey}&searchstring=åkersberga&stationsonly=true`;
 const departureKey = "8ade30c6140f4c08b5ac38690f524819";
 const departureUrl = `https://api.sl.se/api2/realtimedeparturesv4.json?key=${departureKey}&siteid=9662&timewindow=30`;
 const proxyUrl = "https://cors-anywhere.herokuapp.com/"
+
+function JavaScriptFetch() {
+  var script = document.createElement('script');
+  script.src = departureUrl;
+  document.querySelector('head').appendChild(script);
+}
+
+function processJSONPResponse(data) {
+  console.log(data);
+}
+
+
+
 
 /* 
 function reqListener () {
@@ -15,6 +28,7 @@ oReq.open("GET", departureUrl);
 oReq.send();
 */
 
+/* 
 fetch(departureUrl)
 .then((response) => {
   return response.json();
@@ -22,4 +36,4 @@ fetch(departureUrl)
 .then((data) => {
   console.log(data);
 })
-
+ */
